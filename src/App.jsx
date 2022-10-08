6a40c84e3d88fd95016bd69ced211b9034e85261
Marcelo_Bettini <marcelobettini@yahoo.com.ar>
2022-10-08 17:05:37 -0300

    conditional rendering of cards based on ViewPort

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,52 @@
+import React, { useState, useEffect } from 'react';
 import "./App.css";
-
-
+import 'bootstrap/dist/css/bootstrap.min.css';
 import { rovers } from '../rovers/rovers';
 import Rover from "./components/Rover";
+import RoverMobile from "./components/RoverMobile";
+import Header from './components/Header';
+
 import './App.css'
 function App() {
-  return (
-    <>
-      <main className="container">
-        {/* <Rover rover={rovers[0]} /> */}
-        {/* {/* <Rover rover={rovers[1]} /> */}
-        {/* <Rover rover={rovers[2]} /> */}
-        <Rover rover={rovers[3]} />
-      </main>
+  console.log('runs');
+  const [isMobile, setIsMobile] = useState(window.innerWidth < 550);
+  const vwUpdate = () => {
+    setIsMobile(window.innerWidth < 550)
+  }
+  useEffect(() => {
+    window.addEventListener("resize", vwUpdate);
+    return () => window.removeEventListener("resize", vwUpdate);
+  }, []);
 
-    </>
-  );
+  {
+    if (!isMobile) {
+      return (
+        <>
+          <Header />
+          <main className="container">
+            <div className="row justify-content-around my-5">
+              <Rover className="col-12 col-sm-6 " rover={rovers[0]} />
+              <Rover className="col-12 col-sm-6 " rover={rovers[1]} />
+              <Rover className="col-12 col-sm-6 " rover={rovers[2]} />
+              <Rover className="col-12 col-sm-6 " rover={rovers[3]} />
+            </div>
+          </main>
+        </>
+      )
+    } else return (
+      <>
+        <Header />
+        <main className="container">
+          <div className="row justify-content-around my-5">
+            <RoverMobile className="col-12 col-sm-6 " rover={rovers[0]} />
+            <RoverMobile className="col-12 col-sm-6 " rover={rovers[1]} />
+            <RoverMobile className="col-12 col-sm-6 " rover={rovers[2]} />
+            <RoverMobile className="col-12 col-sm-6 " rover={rovers[3]} />
+          </div>
+        </main>
+      </>
+    )
+  }
 }
 
 export default App;
--- src/components/Rover.jsx
+++ src/components/Rover.jsx
@@ -1,19 +1,18 @@
-import React from 'react'
 import Loading from 'react-loading-components'
 import './Rover.css'
 import { useGet } from '../../hooks/useGet';
 import { getFormatedDate } from '../../helpers/getFormatedDate';
 
 const Rover = ({ rover }) => {
+
   const ep = `/${rover.name}/`
-  console.log(ep);
   const [data, error, loading] = useGet(ep)
-  console.log(data);
+
   if (loading) return <Loading type='three_dots' width={100} height={100} fill='#f44242' />
   if (error) return <p>Error loading rover info...</p>
-  return (
 
-    <article className='card'>
+  return (
+    <article className='cardRover my-5'>
       <header className='left'>
         < h2 > {rover.name}</h2 >
         <img src={rover.img} alt={rover.name} />
@@ -25,7 +24,6 @@ const Rover = ({ rover }) => {
       </header>
       <div className="right">
         <ul>
-
           <li><p className='li-title'>Earth departure</p> <p className='li-description'>{new Date(data.rover.launch_date).toDateString()}</p></li>
           <li> <p className='li-title'>Mars arrival</p> <p className='li-description'>{new Date(data.rover.landing_date).toDateString()}</p></li>
           <li> <p className='li-title'>Time on duty</p> <p className='li-description'> {getFormatedDate(data.rover.max_sol)}</p></li>
@@ -35,6 +33,7 @@ const Rover = ({ rover }) => {
 
     </article >
   )
+
 }
 
 export default Rover
