8a3e05169d6efec1e10591788067d3efe168b608
Marcelo_Bettini <marcelobettini@yahoo.com.ar>
2022-10-10 21:12:21 -0300

    public

--- rovers/rovers.js
+++ rovers/rovers.js
@@ -2,22 +2,22 @@ export const rovers = [
   {
     id: 1,
     name: 'spirit',
-    img: "./assets/media/spirit.jpg"
+    img: "./public/media/spirit.jpg"
 
   },
   {
     id: 2,
     name: 'opportunity',
-    img: "./assets/media/opportunity.jpg"
+    img: "./public/media/opportunity.jpg"
   },
   {
     id: 3,
     name: 'curiosity',
-    img: "./assets/media/curiosity.jpg",
+    img: "./public/media/curiosity.jpg",
   },
   {
     id: 4,
     name: 'perseverance',
-    img: "./assets/media/perseverance.jpg"
+    img: "./public/media/perseverance.jpg"
   }
 ]
