61e427005271066ba6500b856bd91ed5afb23576
Marcelo_Bettini <marcelobettini@yahoo.com.ar>
2022-10-10 21:53:58 -0300

    cloudinary

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -6,7 +6,7 @@ const Main = () => {
   return (
     <main id="home">
       <video className='videobg' loop autoPlay muted>
-        <source src='/assets/RoverLanding.mp4' type='video/mp4' />
+        <source src='https://res.cloudinary.com/dsvdu8znq/video/upload/v1665449548/RoverLanding_ltpseo.mp4' type='video/mp4' />
       </video>
       <header className='ttlHeader'>
         <h3>Exploration of Mars</h3>
